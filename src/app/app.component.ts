ab2fb780d865b70e536c8047dee0772e1317b195
VitorGH <vitorbarrosor@gmail.com>
2025-03-22 15:22:00 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule]
 })
 export class AppComponent {
-  title = 'my-pomodoro-app';
+  title = 'Pomodoro Timer';
+  timeLeft: number = 0.1 * 60;
+  breakTime: number = 5 * 60;
+  interval: any;
+  isRunning: boolean = false;
+  breakActive: boolean = false;
+
+  startTimer() {
+    if (!this.isRunning) {
+      this.isRunning = true;
+      this.interval = setInterval(() => {
+        if (this.timeLeft > 0) {
+          this.timeLeft--;
+        } else {
+          this.pauseTimer();
+          alert('Tempo acabou! Faça uma pausa.');
+        }
+      }, 1000);
+    }
+  }
+
+  pauseTimer() {
+    this.isRunning = false;
+    clearInterval(this.interval);
+  }
+
+  resetTimer() {
+    this.pauseTimer();
+    this.timeLeft = this.breakActive ? 5 * 60: 25 * 60;
+  }
+
+  toogleBreak(){
+    this.breakActive = !this.breakActive;
+    this.timeLeft = this.breakActive ? 5 * 60: 25 * 60;
+  }
+
+  // Método para formatar o tempo em MM:SS
+  formatTime(): string {
+    const minutes = Math.floor(this.timeLeft / 60);
+    const seconds = this.timeLeft % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
 }
